bdfacebf04ab7db674c6861b1124bea0e8c4896c
Jonah Snider <jonah@jonahsnider.com>
2024-07-17 15:43:01 -0700

    Fix Brew env init

--- private_dot_config/fish/modules/env.fish
+++ private_dot_config/fish/modules/env.fish
@@ -54,7 +54,12 @@ set -gx DOTNET_CLI_TELEMETRY_OPTOUT true
 
 #region Brew
 if test -f /opt/homebrew/bin/brew
-  eval (/opt/homebrew/bin/brew shellenv)
+  set -gx HOMEBREW_PREFIX "/opt/homebrew"
+	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
+  fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
+  ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+  ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
 end
 #endregion
 
