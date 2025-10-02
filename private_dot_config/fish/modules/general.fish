3e20e055af435ab89ceda1833f4dbb8719fe3f36
Jonah Snider <jonah@jonahsnider.com>
2025-10-02 13:25:20 -0700

    Remove unused abbreviations

--- private_dot_config/fish/modules/general.fish
+++ private_dot_config/fish/modules/general.fish
@@ -1,13 +1,3 @@
-#region Python
-# Use Python 3 by default
-abbr --add python python3
-#endregion
-
-#region Yarn
-# Alias Squoosh CLI
-abbr --add squoosh squoosh-cli
-#endregion
-
 # 1Password plugins
 source ~/.config/op/plugins.sh
 
