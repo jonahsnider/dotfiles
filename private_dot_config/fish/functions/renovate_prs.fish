1360e2ac0a8ea686d8d97246064a3d6a8fc0101a
Jonah Snider <jonah@jonahsnider.com>
2026-08-03 14:38:38 -0700

    Improve robustness of renovate_prs
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- private_dot_config/fish/functions/renovate_prs.fish
+++ private_dot_config/fish/functions/renovate_prs.fish
@@ -1,16 +1,55 @@
 function renovate_prs -d "List open Renovate PRs with passing checks as Markdown"
     set -l width $COLUMNS
+    set -l query 'query {
+  personal: search(
+    query: "is:pr is:open user:jonahsnider archived:false author:app/jonahsnider"
+    type: ISSUE
+    first: 100
+  ) {
+    nodes { ...pullRequestFields }
+  }
+  team581: search(
+    query: "is:pr is:open org:team581 archived:false author:app/renovate"
+    type: ISSUE
+    first: 100
+  ) {
+    nodes { ...pullRequestFields }
+  }
+  zwsIm: search(
+    query: "is:pr is:open org:zws-im archived:false author:app/renovate"
+    type: ISSUE
+    first: 100
+  ) {
+    nodes { ...pullRequestFields }
+  }
+}
 
-    gh search prs \
-        --author app/renovate \
-        --state open \
-        --owner jonahsnider \
-        --archived=false \
-        --checks success \
-        --limit 100 \
-        --json title,url,repository,number \
-        --template '# Renovate PRs with passing checks
+fragment pullRequestFields on PullRequest {
+  title
+  url
+  number
+  repository {
+    nameWithOwner
+  }
+  commits(last: 1) {
+    nodes {
+      commit {
+        statusCheckRollup {
+          state
+        }
+      }
+    }
+  }
+}'
 
-{{range .}}- [{{.repository.nameWithOwner}}#{{.number}}]({{.url}}) — {{.title}}
-{{end}}' | glow --width $width -
+    begin
+        echo '# Renovate PRs with passing checks'
+        echo
+
+        gh api graphql \
+            -f query="$query" \
+            --jq '.data[].nodes[]
+                | select(.commits.nodes[-1].commit.statusCheckRollup.state == "SUCCESS")
+                | "- [\(.repository.nameWithOwner)#\(.number)](\(.url)) — \(.title)"'
+    end | glow --width $width -
 end
